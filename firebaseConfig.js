1fb9533fef5c20012f7d5ce959bc3a1c1b1f25b5
Heechan <ckstmznf@naver.com>
2022-11-12 22:36:38 +0900

    Add:config

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCwKkLssbQEvYD0qUYYQ_s2-Hq-JSp-Vac",
+    authDomain: "edcan-web-2022.firebaseapp.com",
+    projectId: "edcan-web-2022",
+    storageBucket: "edcan-web-2022.appspot.com",
+    messagingSenderId: "399765369943",
+    appId: "1:399765369943:web:4a2d983742bf69e5dd5f12",
+    measurementId: "G-LWTRKLXV3S"
+};
